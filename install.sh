3e5fcf0a270d05609b88b3caec621584780049f9
Yosh Muhaimin <146873008+yoshmultimedia@users.noreply.github.com>
2023-10-12 06:32:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ fi
 cp ngrok.service /lib/systemd/system/
 mkdir -p /opt/ngrok
 cp ngrok.yml /opt/ngrok
-sed -i "s/<add_your_token_here>/$1/g" /opt/ngrok/ngrok.yml
+sed -i "s/<add_your_token_here>/$1/g" /root/.config/ngrok/ngrok.yml
 
 cd /opt/ngrok
 echo "Downloading ngrok for $ARCH . . ."
